727f3eb62cbc8e12717c0ba47f640dee0fea1dc6
Yago Nobre <nobre.yago@gmail.com>
2018-11-18 01:52:27 -0200

    Add update-instances.sh

---
+++ hack/update-instances.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+curl -Lo hack/instances.json https://www.ec2instances.info/instances.json
+
+if [[ `git status --porcelain` ]]; then
+	  git checkout master
+    go generate
+
+    git config user.name "Batata.Bot"
+    git config user.email "batata.the.bot@gmail.com"
+
+    git add instances_data.go
+    git add hack/instances.json
+    git commit -m "Update instances info"
+
+    VERSION=`git describe --tags $(git rev-list --tags --max-count=1) | awk -F. '{$NF+=1; OFS="."; print $0}'| sed 's/ /./g'`
+    git tag ${VERSION}
+
+    git remote add github https://${GH_TOKEN}@${GH_REF} > /dev/null 2>&1
+    git push github master
+    git push github ${VERSION}
+fi
